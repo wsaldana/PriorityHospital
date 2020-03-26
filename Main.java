8568056411bf5608f4a0e8955c58aad45185e9e2
wsaldana <53311124+wsaldana@users.noreply.github.com>
2020-03-26 11:23:22 -0600

    Create Main class

---
+++ Main.java
@@ -0,0 +1,12 @@
+/**
+ * @author Walter Saldaña #19897
+ * @since 26/03/2020
+ * Programa que simula una lista de atencion a clientes en un hospital
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
