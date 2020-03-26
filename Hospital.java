6af064251aa7a7562dd76f041c934581755dc54c
wsaldana <53311124+wsaldana@users.noreply.github.com>
2020-03-26 12:05:17 -0600

    Create controller

---
+++ Hospital.java
@@ -0,0 +1,11 @@
+/**
+ * @author Walter Saldaña #19897
+ * Clase que realiza las operaciones entre las clases del Hospital
+ */
+public class Hospital {
+
+    /**
+     * Constructor sin parametros
+     */
+    public Hospital(){}
+}
