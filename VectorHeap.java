35ea7bf5d4daf91c4072ec0fffe2f6bc95b4862f
wsaldana <53311124+wsaldana@users.noreply.github.com>
2020-03-26 12:05:49 -0600

    Create implementation class

---
+++ VectorHeap.java
@@ -0,0 +1,51 @@
+/**
+ * @author Walter Saldaña #19897
+ * @since 26/03/2020
+ * VectorHeap, cola que implementa un PriorityQueue
+ */
+
+public class VectorHeap<E  extends Comparable<E>> implements iPriorityQueue<E>{
+
+    /**
+     * Metodo constructor sin parametros
+     * Crea un objeto VectorHeap
+     */
+    public VectorHeap(){}
+
+    @Override
+    public E getFirst() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public E remove() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void add(E value) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
