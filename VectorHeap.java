02dbadc3dc4982e75150e3dd805f74953572a108
wsaldana <53311124+wsaldana@users.noreply.github.com>
2020-04-05 23:49:18 -0600

    Javadoc

--- VectorHeap.java
+++ VectorHeap.java
@@ -16,6 +16,9 @@ public class VectorHeap<E  extends Comparable<E>> implements iPriorityQueue<E>{
         this.data = new Vector<E>();
     }
 
+    /**
+     * @return valor minimo en el priority queue
+     */
     @Override
     public E getFirst() {
         if(data.size()>0){
@@ -25,25 +28,32 @@ public class VectorHeap<E  extends Comparable<E>> implements iPriorityQueue<E>{
         }
     }
 
+    /**
+     * Elimina el valor minimo
+     * @return valor minimo antes de eliminarlo
+     */
     @Override
     public E remove() {
         return data.remove(0);
     }
 
+    /**
+     * Agrega un valor a la priority queue
+     * @param value nuevo valor a agregar
+     */
     @Override
     public void add(E value) {
+        //Si no hay mas elementos, solo so agrega
         if(data.isEmpty()){
             data.add(value);
+            //Si ya hay elementos, verificar donde colocarlo
         }else{
             int index = 0;
-            //for(int i=0; i<data.size()-1; i++){
-            //    if((value.compareTo(data.get(index)) < 0 && index == -1) || ){
-            //        index = i;
-            //    }
-            //}
+            //Buscar el indice en el que pertenece
             while(value.compareTo(data.get(index)) > 0 && index < data.size()){
                 index++;
             }
+            //Crear nuevo vector agregando el valor nuevo
             Vector<E> newData = new Vector<E>();
             for(int i=0; i<data.size(); i++){
                 if(i != index){
@@ -53,20 +63,31 @@ public class VectorHeap<E  extends Comparable<E>> implements iPriorityQueue<E>{
                     newData.add(data.get(i));
                 }
             }
+            //Cambiar el viejo vector con el nuevo que tiene el item agergado
             this.data = newData;
         }
     }
 
+    /**
+     * @return true si no hay elementos en el priority queue
+     * @return false si hay elementos en el priority queue
+     */
     @Override
     public boolean isEmpty() {
         return data.isEmpty();
     }
 
+    /**
+     * @return numero de elementos guardados
+     */
     @Override
     public int size() {
         return data.size();
     }
 
+    /**
+     * Elimina todos los elementos 
+     */
     @Override
     public void clear() {
         data.clear();
